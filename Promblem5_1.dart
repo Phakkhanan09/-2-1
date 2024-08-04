4a167b27145b2c2d0d836722f0000c18f77c19ab
Phakkhanan09 <Phakkhanan.nak@gmail.com>
2024-08-04 16:11:41 +0700

    LAB1_5_2
    LAB1_5_2

---
+++ Promblem5_1.dart
@@ -0,0 +1,25 @@
+void main(){
+    print ( count_base("AAAATTTGGC"));
+    print ( count_base("ATTGGC"));
+    print ( count_base("TTTATCCCTGGCAAA"));
+}
+List<int> count_base(String dna){
+    List<int> atgc = [0,0,0,0];
+
+    for (int i = 0; i < dna.length; i++){
+      
+      //String base = dna[i]; //เก็บค่า dna ในตำแหน่งที่ i ไว้ที่ ตัวแปร base
+       
+       if (dna[i] == 'A'){
+            atgc[0] += 1;
+       }else if (dna[i] == 'T'){
+            atgc[1] += 1;
+       }else if (dna[i] == 'G'){
+            atgc[2] += 1;
+       }else if (dna[i] == 'C'){
+            atgc[3] += 1;
+       }
+    }
+
+    return atgc;
+}
